fee0e852145b4f60336ba05d5eff97b295541e48
silveriolaridev <larissa.silverio@sptech.school>
2024-11-23 17:25:29 -0300

    adicionando estilos

---
+++ public/js/estilos.js
@@ -0,0 +1,19 @@
+function mostrarEstilo(estiloId, botaoId ) {
+
+    let estilos = document.getElementsByClassName('estilo');
+  
+    for (var i = 0; i < estilos.length; i++) {
+      estilos[i].style.display = 'none';
+    }
+    
+    document.getElementById(estiloId).style.display = 'flex';
+
+    let botoes = document.getElementsByClassName('botao-estilo');
+  
+    for (var i = 0; i < estilos.length; i++) {
+      botoes[i].style.border = 'none';
+    }
+
+    document.getElementById(botaoId).style.border = 'solid 2px white';
+  }
+  
